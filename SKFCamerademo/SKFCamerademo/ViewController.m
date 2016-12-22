c40210cfaf1cb0fbe9ec7c0d058138f47acae18f
孙凯峰 <sunkaifeng@sunkaifengdeMacBook-Pro.local>
2016-12-22 12:09:13 +0800

    test git hook

--- SKFCamerademo/SKFCamerademo/ViewController.m
+++ SKFCamerademo/SKFCamerademo/ViewController.m
@@ -20,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     if (YES) {NSLog(@"乱的代码");}
+    
     //乱的代码
     [self performSelector:@selector(testcode) withObject:self afterDelay:3];
     // Do any additional setup after loading the view, typically from a nib.
