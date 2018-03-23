568ef64c6c43ef9344a1f6a1121d0aa36ff7aa45
孙凯峰 <>
2018-03-23 17:31:32 +0800

    测试提交20180323

---
+++ PureCamerademo/PureCamerademo/KModel.h
@@ -0,0 +1,13 @@
+//
+//  KModel.h
+//  PureCamerademo
+//
+//  Created by 五月 on 2018/3/23.
+//  Copyright © 2018年 五月. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KModel : NSObject
+
+@end
---
+++ PureCamerademo/PureCamerademo/KModel.m
@@ -0,0 +1,13 @@
+//
+//  KModel.m
+//  PureCamerademo
+//
+//  Created by 五月 on 2018/3/23.
+//  Copyright © 2018年 五月. All rights reserved.
+//
+
+#import "KModel.h"
+
+@implementation KModel
+
+@end
